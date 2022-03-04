c151f8adb940265fb789549f413cadb61c0c3a3d
Neil Francis Hipona <neil.francis.hipona@codeandtheory.com>
2022-03-04 11:21:03 +0800

    Initial Commit

---
+++ SwiftUITraining/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITraining
+//
+//  Created by Neil Francis Hipona on 3/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUITraining/SwiftUITrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITrainingApp.swift
+//  SwiftUITraining
+//
+//  Created by Neil Francis Hipona on 3/4/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
