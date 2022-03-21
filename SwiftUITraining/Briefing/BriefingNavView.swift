5836ceeb9a4b1ffecd513c4e9f2ca70976d1201c
Neil Francis Hipona <neil.francis.hipona@codeandtheory.com>
2022-03-21 18:01:40 +0800

    INP - briefing navigation

---
+++ SwiftUITraining/Briefing/BriefingNavView.swift
@@ -0,0 +1,20 @@
+//
+//  BriefingNavView.swift
+//  SwiftUITraining
+//
+//  Created by Neil Francis Hipona on 3/21/22.
+//
+
+import SwiftUI
+
+struct BriefingNavView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct BriefingNavView_Previews: PreviewProvider {
+    static var previews: some View {
+        BriefingNavView()
+    }
+}
--- SwiftUITraining/Briefing/BriefingView.swift
+++ SwiftUITraining/Briefing/BriefingView.swift
@@ -10,11 +10,19 @@ import SwiftUI
 struct BriefingView: View {
     var body: some View {
         GeometryReader { geometry in
-            ScrollView {
-                Image("img_briefing_content")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: abs(geometry.size.width), alignment: .center)
+            ZStack {
+                ScrollView {
+                    Image("img_briefing_content")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: abs(geometry.size.width), alignment: .center)
+                }
+
+                VStack {
+                    Spacer()
+                    BriefingNavView()
+                        .padding(.bottom, 40)
+                }
             }
         }
         .padding(.top, 20)
