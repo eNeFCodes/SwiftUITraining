e2d4feb79eab7e6f8578a0f65b6a8268fec06fdd
Neil Francis Hipona <neil.francis.hipona@codeandtheory.com>
2022-03-24 11:39:01 +0800

    Fix InputFieldView.swift layout in nested views

--- SwiftUITraining/Reusables/InputFieldView.swift
+++ SwiftUITraining/Reusables/InputFieldView.swift
@@ -14,7 +14,6 @@ struct InputFieldView: View {
     class Config: Identifiable, ObservableObject {
         let id: Int
         @Published var text: String
-        let geometry: GeometryProxy
 
         let placeholder: String
         let placeholderTextColor: Color
@@ -29,7 +28,6 @@ struct InputFieldView: View {
 
         init(id: Int,
              text: String = "",
-             geometry: GeometryProxy,
 
              placeholder: String = "Search CNT",
              placeholderTextColor: Color = .gray,
@@ -44,7 +42,6 @@ struct InputFieldView: View {
 
             self.id = id
             self.text = text
-            self.geometry = geometry
 
             self.placeholder = placeholder
             self.placeholderTextColor = placeholderTextColor
@@ -71,45 +68,44 @@ struct InputFieldView: View {
     @StateObject var config: Config
     
     var body: some View {
-        VStack(spacing: 10) {
-            HStack(alignment: .center, spacing: 10) {
-                ZStack(alignment: .leading) {
-                    if config.text.isEmpty {
-                        Text(config.placeholder)
-                            .accessibilityLabel(config.placeholder)
-                            .foregroundColor(config.placeholderTextColor)
-                    }
-                    TextField("", text: $config.text)
-                        .frame(height: 40, alignment: .leading)
-                        .foregroundColor(config.textColor)
-                        .onChange(of: config.text) { newValue in
-                            config.onChange()
+        GeometryReader { geometry in
+            VStack(spacing: 10) {
+                HStack(alignment: .center, spacing: 10) {
+                    ZStack(alignment: .leading) {
+                        if config.text.isEmpty {
+                            Text(config.placeholder)
+                                .accessibilityLabel(config.placeholder)
+                                .foregroundColor(config.placeholderTextColor)
                         }
+                        TextField("", text: $config.text)
+                            .frame(height: 40, alignment: .leading)
+                            .foregroundColor(config.textColor)
+                            .onChange(of: config.text) { newValue in
+                                config.onChange()
+                            }
 
-                }
-                .padding(.leading, 20)
-
-                if config.showButton, let icon = config.icon {
-                    Button {
-                        config.trigger()
-                    } label: {
-                        icon
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 40, height: 40, alignment: .center)
                     }
-                    .padding(.trailing, 20)
+
+                    if config.showButton, let icon = config.icon {
+                        Button {
+                            config.trigger()
+                        } label: {
+                            icon
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 40, height: 40, alignment: .center)
+                        }
+                    }
+                }
+
+                if config.showSeparator {
+                    let p1 = CGPoint(x: 0, y: 0)
+                    let p2 = CGPoint(x: abs(geometry.size.width), y: 0)
+                    BorderView(coordinates: [p1, p2])
+                        .stroke(config.separatorColor, lineWidth: 1)
                 }
             }
-
-            if config.showSeparator {
-                let p1 = CGPoint(x: 0, y: 0)
-                let p2 = CGPoint(x: config.geometry.size.width - 40, y: 0)
-                BorderView(coordinates: [p1, p2])
-                    .stroke(config.separatorColor, lineWidth: 1)
-                    .padding(.leading, 20)
-                    .padding(.trailing, 20)
-            }
+            .frame(width: geometry.size.width, height: 51, alignment: .center)
         }
         .frame(height: 51, alignment: .center)
     }
@@ -117,39 +113,34 @@ struct InputFieldView: View {
 
 struct InputFieldView_Previews: PreviewProvider {
     static var previews: some View {
-        GeometryReader { geometry in
-            VStack {
-                let config = InputFieldView.Config(id: 0,
-                                                   text: "",
-                                                   geometry: geometry,
-                                                   showButton: true,
-                                                   icon: Image("ic_search_white"),
-                                                   action: triggerInputFieldAction)
+        VStack {
+            let config = InputFieldView.Config(id: 0,
+                                               text: "",
+                                               showButton: true,
+                                               icon: Image("ic_search_white"),
+                                               action: triggerInputFieldAction)
 
-                let config2 = InputFieldView.Config(id: 1,
-                                                    text: "",
-                                                    geometry: geometry,
-                                                    separatorColor: .yellow,
-                                                    showButton: true,
-                                                    icon: Image("ic_search"),
-                                                    action: triggerInputFieldAction)
+            let config2 = InputFieldView.Config(id: 1,
+                                                text: "",
+                                                separatorColor: .yellow,
+                                                showButton: true,
+                                                icon: Image("ic_search"),
+                                                action: triggerInputFieldAction)
 
-                let config3 = InputFieldView.Config(id: 3,
-                                                    text: "",
-                                                    geometry: geometry,
-                                                    showSeparator: false,
-                                                    showButton: true,
-                                                    icon: Image("ic_eye_white"),
-                                                    action: triggerInputFieldAction)
+            let config3 = InputFieldView.Config(id: 3,
+                                                text: "",
+                                                showSeparator: false,
+                                                showButton: true,
+                                                icon: Image("ic_eye_white"),
+                                                action: triggerInputFieldAction)
 
-                ForEach([config, config2, config3], id: \.id) { config in
-                    InputFieldView(config: config)
-                }
-
-                Spacer()
+            ForEach([config, config2, config3], id: \.id) { config in
+                InputFieldView(config: config)
             }
-            .background(Color.black)
+
+            Spacer()
         }
+        .background(Color.black)
     }
 
     private static func triggerInputFieldAction(id: Int, text: String) {
