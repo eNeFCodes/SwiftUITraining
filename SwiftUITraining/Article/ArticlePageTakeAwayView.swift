275e8c4cc3df69dcba19b4579abd0f993c370ae9
Neil Francis Hipona <neil.francis.hipona@codeandtheory.com>
2022-03-28 19:03:23 +0800

    Update take away view layout

--- SwiftUITraining/Article/ArticlePageTakeAwayView.swift
+++ SwiftUITraining/Article/ArticlePageTakeAwayView.swift
@@ -20,6 +20,7 @@ extension ArticlePageTakeAwayView {
 
         let miniTitle: String
         let title: String
+        let image: Image?
         let infoDate: String
         let infoAuthor: String
         let infoLocation: String
@@ -27,8 +28,14 @@ extension ArticlePageTakeAwayView {
         let takeAwayTitle: String
         let takeAways: [TakeAway]
 
+        var hasImage: Bool {
+            guard let _ = image else { return false }
+            return true
+        }
+
         init(miniTitle: String,
              title: String,
+             image: Image? = nil,
              infoDate: String,
              infoAuthor: String,
              infoLocation: String,
@@ -38,6 +45,7 @@ extension ArticlePageTakeAwayView {
 
             self.miniTitle = miniTitle
             self.title = title
+            self.image = image
             self.infoDate = infoDate
             self.infoAuthor = infoAuthor
             self.infoLocation = infoLocation
@@ -50,148 +58,172 @@ extension ArticlePageTakeAwayView {
 
 struct ArticlePageTakeAwayView: View {
 
-    let model: Model = ArticlePageView.mockTakeAwayData()
+//    let model: Model = ArticlePageView.mockTakeAwayData()
+    let model: Model = ArticlePageView.mockTakeAwayImageData()
 
     var body: some View {
-        GeometryReader { geometry in
-            ZStack {
-                VStack {
-                    Image("img_bg_article")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: geometry.size.width, height: 200, alignment: .bottom)
-                        .clipped()
-                    Spacer()
-                }
-                .ignoresSafeArea()
-
-                VStack(alignment: .leading, spacing: 18) {
+        VStack {
+            GeometryReader { geometry in
+                ZStack {
                     // title stack
-                    VStack(alignment: .leading, spacing: 8) {
-                        let estimatedWidth = geometry.size.width - 40
+                    let titleFont = UIFont(name: "BrilliantCutProB7-Bold", size: 28)!
+                    let estimatedWidth = geometry.size.width - 40
+                    let headerTopPadding: CGFloat = model.hasImage ? 265 : 91
+                    let headerBotPadding: CGFloat = model.hasImage ? 40 : 0
+                    let titleHeight = model.title.constrainedSize(with: .init(width: estimatedWidth, height: .infinity),
+                                                                  minHeight: 28,
+                                                                  attributes: [.font: titleFont])
 
-                        let miniTitleFont = Font.custom("BrilliantCutProB7-Bold", size: 11)
-                        Text(model.miniTitle)
-                            .accessibilityLabel(model.miniTitle)
-                            .font(miniTitleFont)
-                            .foregroundColor(.black)
-
-
-                        let titleFont = UIFont(name: "BrilliantCutProB7-Bold", size: 28)!
-                        let titleHeight = model.title.constrainedSize(with: .init(width: estimatedWidth, height: .infinity),
-                                                                            minHeight: 28,
-                                                                            attributes: [.font: titleFont])
-                        Text(model.title)
-                            .accessibilityLabel(model.title)
-                            .font(titleFont.toFont())
-                            .foregroundColor(.black)
-                            .frame(width: abs(estimatedWidth), height: titleHeight, alignment: .leading)
+                    VStack {
+                        if let image = model.image {
+                            let bgImageFrameHeight = 20 + headerTopPadding + 15 + 8 + titleHeight + headerBotPadding
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: geometry.size.width, height: bgImageFrameHeight, alignment: .bottom)
+                                .clipped()
+                        } else {
+                            let bgImageFrameHeight = headerTopPadding + 15 + 8 + titleHeight - 20
+                            Image("img_bg_article")
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: geometry.size.width, height: bgImageFrameHeight, alignment: .bottom)
+                                .clipped()
+                        }
+                        Spacer()
                     }
 
-                    // detail stack
-                    Group {
-                        let estimatedWidth = geometry.size.width - 65
+                    VStack(alignment: .leading, spacing: 18) {
+                        // title stack
+                        VStack(alignment: .leading, spacing: 8) {
+                            let miniTitleFont = Font.custom("BrilliantCutProB7-Bold", size: 11)
+                            Text(model.miniTitle)
+                                .accessibilityLabel(model.miniTitle)
+                                .font(miniTitleFont)
+                                .frame(width: abs(estimatedWidth), height: 15, alignment: .leading)
 
-                        let infoDateFont = UIFont(name: "FancyCutProB7-Bold", size: 14)!
-                        let infoDateHeight = model.infoDate.constrainedSize(with: .init(width: estimatedWidth, height: .infinity),
-                                                                            minHeight: 15,
-                                                                            attributes: [.font: infoDateFont])
-
-                        let infoAuthorFont = UIFont(name: "FancyCutProB7-Regular", size: 14)!
-                        let infoAuthorHeight = model.infoAuthor.constrainedSize(with: .init(width: estimatedWidth, height: .infinity),
-                                                                                minHeight: 15,
-                                                                                attributes: [.font: infoAuthorFont])
-
-                        let infoLocationFont = UIFont(name: "FancyCutProB7-Bold", size: 14)!
-                        let infoLocationHeight = model.infoLocation.constrainedSize(with: .init(width: estimatedWidth, height: .infinity),
-                                                                                    minHeight: 15,
-                                                                                    attributes: [.font: infoLocationFont])
-
-                        let infoDetailFont = UIFont(name: "FancyCutProB7-Light", size: 29)!
-                        let infoDetailHeight = model.infoDetail.constrainedSize(with: .init(width: estimatedWidth, height: .infinity),
-                                                                                minHeight: 30,
-                                                                                attributes: [.font: infoDetailFont])
-                        // 8 * 3 + 24
-                        let canvasHeight = infoDateHeight + infoAuthorHeight + infoLocationHeight + 48 + infoDetailHeight
-
-                        HStack(alignment: .top, spacing: 24) {
-                            VStack {
-                                GeometryReader { geometry in
-                                    let p1 = CGPoint(x: 0, y: 0)
-                                    let p2 = CGPoint(x: 0, y: geometry.size.height)
-                                    BorderView(coordinates: [p1, p2])
-                                        .stroke(style: StrokeStyle(lineWidth: 1, dash: [2]))
-                                        .foregroundColor(Color.init(hex: "AC9852"))
-                                        .frame(width: 1, height: geometry.size.height, alignment: .leading)
-                                }
-                            }
-                            .frame(width: 1, alignment: .leading)
-
-                            VStack(alignment: .leading, spacing: 8) {
-                                Text(model.infoDate)
-                                    .accessibilityLabel(model.infoDate)
-                                    .font(infoDateFont.toFont())
-                                    .foregroundColor(.black)
-                                Text(model.infoAuthor)
-                                    .accessibilityLabel(model.infoAuthor)
-                                    .font(infoAuthorFont.toFont())
-                                    .foregroundColor(.black)
-                                Text(model.infoLocation)
-                                    .accessibilityLabel(model.infoLocation)
-                                    .font(infoLocationFont.toFont())
-                                    .foregroundColor(.black)
-
-                                Spacer()
-
-                                Text(model.infoDetail)
-                                    .accessibilityLabel(model.infoDetail)
-                                    .font(infoDetailFont.toFont())
-                                    .foregroundColor(.black)
-                            }
+                            Text(model.title)
+                                .accessibilityLabel(model.title)
+                                .font(titleFont.toFont())
+                                .frame(width: abs(estimatedWidth), height: titleHeight, alignment: .leading)
                         }
-                        .frame(height: canvasHeight, alignment: .center)
+                        .foregroundColor(model.hasImage ? .white : .black)
+                        .padding(.top, headerTopPadding)
+                        .padding(.bottom, headerBotPadding)
+
+                        // detail stack
+                        detailStack(geometry: geometry)
+
+                        // takeaways
+                        takeAwayStack(geometry: geometry)
                     }
+                    .padding(20)
+                    .frame(width: abs(geometry.size.width))
+                }
+                .frame(width: abs(geometry.size.width), alignment: .top)
+                .background(Color.init(hex: "DCD7CA"))
+            }
+        }
+    }
 
-                    // takeaways
-                    Group {
-                        let takeawayTitleFont = Font.custom("BrilliantCutProB7-Bold", size: 11)
-                        Text(model.takeAwayTitle)
-                            .accessibilityLabel(model.takeAwayTitle)
-                            .font(takeawayTitleFont)
-                            .foregroundColor(.black)
-                            .padding(.top, 30)
+    private func detailStack(geometry: GeometryProxy) -> some View {
+        Group {
+            let estimatedWidth = geometry.size.width - 65
 
-                        VStack(spacing: 24) {
-                            let estimatedWidth = geometry.size.width - 98
-                            let numberFont = Font.custom("FancyCutCondProB7-Bold", size: 44)
-                            let takeAwayDFont = UIFont(name: "FancyCutProB7-Regular", size: 18)!
+            let infoDateFont = UIFont(name: "FancyCutProB7-Bold", size: 14)!
+            let infoDateHeight = model.infoDate.constrainedSize(with: .init(width: estimatedWidth, height: .infinity),
+                                                                minHeight: 15,
+                                                                attributes: [.font: infoDateFont])
 
-                            ForEach(model.takeAways, id: \.id) { takeAway in
-                                let takeAwayDetailHeight = takeAway.detail.constrainedSize(with: .init(width: estimatedWidth, height: .infinity),
-                                                                                           minHeight: 50,
-                                                                                           attributes: [.font: takeAwayDFont])
-                                HStack(spacing: 8) {
-                                    Text(takeAway.number)
-                                        .accessibilityLabel(takeAway.number)
-                                        .foregroundColor(Color.init(hex: "C10016"))
-                                        .font(numberFont)
-                                        .frame(width: 50, alignment: .center)
+            let infoAuthorFont = UIFont(name: "FancyCutProB7-Regular", size: 14)!
+            let infoAuthorHeight = model.infoAuthor.constrainedSize(with: .init(width: estimatedWidth, height: .infinity),
+                                                                    minHeight: 15,
+                                                                    attributes: [.font: infoAuthorFont])
 
-                                    Text(takeAway.detail)
-                                        .accessibilityLabel(takeAway.detail)
-                                        .foregroundColor(.black)
-                                        .font(takeAwayDFont.toFont())
-                                        .frame(width: abs(estimatedWidth), height: abs(takeAwayDetailHeight), alignment: .leading)
-                                }
-                            }
-                        }
+            let infoLocationFont = UIFont(name: "FancyCutProB7-Bold", size: 14)!
+            let infoLocationHeight = model.infoLocation.constrainedSize(with: .init(width: estimatedWidth, height: .infinity),
+                                                                        minHeight: 15,
+                                                                        attributes: [.font: infoLocationFont])
+
+            let infoDetailFont = UIFont(name: "FancyCutProB7-Light", size: 29)!
+            let infoDetailHeight = model.infoDetail.constrainedSize(with: .init(width: estimatedWidth, height: .infinity),
+                                                                    minHeight: 30,
+                                                                    attributes: [.font: infoDetailFont])
+            // 8 * 3 + 24
+            let canvasHeight = infoDateHeight + infoAuthorHeight + infoLocationHeight + 48 + infoDetailHeight
+
+            HStack(alignment: .top, spacing: 24) {
+                VStack {
+                    GeometryReader { geometry in
+                        let p1 = CGPoint(x: 0, y: 0)
+                        let p2 = CGPoint(x: 0, y: geometry.size.height)
+                        BorderView(coordinates: [p1, p2])
+                            .stroke(style: StrokeStyle(lineWidth: 1, dash: [2]))
+                            .foregroundColor(Color.init(hex: "AC9852"))
+                            .frame(width: 1, height: geometry.size.height, alignment: .leading)
                     }
                 }
-                .padding(20)
-                .frame(width: abs(geometry.size.width))
+                .frame(width: 1, alignment: .leading)
+
+                VStack(alignment: .leading, spacing: 8) {
+                    Text(model.infoDate)
+                        .accessibilityLabel(model.infoDate)
+                        .font(infoDateFont.toFont())
+                        .foregroundColor(.black)
+                    Text(model.infoAuthor)
+                        .accessibilityLabel(model.infoAuthor)
+                        .font(infoAuthorFont.toFont())
+                        .foregroundColor(.black)
+                    Text(model.infoLocation)
+                        .accessibilityLabel(model.infoLocation)
+                        .font(infoLocationFont.toFont())
+                        .foregroundColor(.black)
+
+                    Spacer()
+
+                    Text(model.infoDetail)
+                        .accessibilityLabel(model.infoDetail)
+                        .font(infoDetailFont.toFont())
+                        .foregroundColor(.black)
+                }
             }
-            .frame(width: abs(geometry.size.width), alignment: .top)
-            .background(Color.init(hex: "DCD7CA"))
+            .frame(height: canvasHeight, alignment: .center)
+        }
+    }
+
+    private func takeAwayStack(geometry: GeometryProxy) -> some View {
+        Group {
+            let takeawayTitleFont = Font.custom("BrilliantCutProB7-Bold", size: 11)
+            Text(model.takeAwayTitle)
+                .accessibilityLabel(model.takeAwayTitle)
+                .font(takeawayTitleFont)
+                .foregroundColor(.black)
+                .padding(.top, 30)
+
+            VStack(spacing: 24) {
+                let estimatedWidth = geometry.size.width - 98
+                let numberFont = Font.custom("FancyCutCondProB7-Bold", size: 44)
+                let takeAwayDFont = UIFont(name: "FancyCutProB7-Regular", size: 18)!
+
+                ForEach(model.takeAways, id: \.id) { takeAway in
+                    let takeAwayDetailHeight = takeAway.detail.constrainedSize(with: .init(width: estimatedWidth, height: .infinity),
+                                                                               minHeight: 50,
+                                                                               attributes: [.font: takeAwayDFont])
+                    HStack(spacing: 8) {
+                        Text(takeAway.number)
+                            .accessibilityLabel(takeAway.number)
+                            .foregroundColor(Color.init(hex: "C10016"))
+                            .font(numberFont)
+                            .frame(width: 50, alignment: .center)
+
+                        Text(takeAway.detail)
+                            .accessibilityLabel(takeAway.detail)
+                            .foregroundColor(.black)
+                            .font(takeAwayDFont.toFont())
+                            .frame(width: abs(estimatedWidth), height: abs(takeAwayDetailHeight), alignment: .leading)
+                    }
+                }
+            }
         }
     }
 }
--- SwiftUITraining/Article/ArticlePageView.swift
+++ SwiftUITraining/Article/ArticlePageView.swift
@@ -13,7 +13,6 @@ struct ArticlePageView: View {
     var body: some View {
         ScrollView(.vertical) {
             ArticlePageTakeAwayView()
-                .padding(.top, 91)
         }
         .ignoresSafeArea()
         .background(Color.white)
@@ -48,6 +47,26 @@ extension ArticlePageView {
     static func mockTakeAwayData() -> ArticlePageTakeAwayView.Model {
         let model: ArticlePageTakeAwayView.Model = .init(miniTitle: "HISTORY",
                                                          title: "Trinity, a Mainson emblem and jewelery icon".uppercased(),
+                                                         infoDate: "APR 02, 2020",
+                                                         infoAuthor: "Jane Doe, Lorem Ispum Role",
+                                                         infoLocation: "5th Avenue Mansion",
+                                                         infoDetail: "Created in 1924 by Louis Cartier, the Trinity ring soon became one of the emblems of the Maison and a jewelery icon.",
+                                                         takeAways: [
+                                                            .init(id: 1,
+                                                                  detail: "First ring made of three interlaced individual bands. Lorem ipsum dolor sit amet, consectetur adipiscing elit."),
+                                                            .init(id: 2,
+                                                                  detail: "The harmonious interplay between the bands create an expression of attachment. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex."),
+                                                            .init(id: 3,
+                                                                  detail: "Relaunched by Cartier in 1981. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.")
+                                                         ])
+
+        return model
+    }
+
+    static func mockTakeAwayImageData() -> ArticlePageTakeAwayView.Model {
+        let model: ArticlePageTakeAwayView.Model = .init(miniTitle: "HISTORY",
+                                                         title: "Trinity, a Mainson emblem and jewelery icon".uppercased(),
+                                                         image: Image("img_bg_article1"),
                                                          infoDate: "APR 02, 2020",
                                                          infoAuthor: "Jane Doe, Lorem Ispum Role",
                                                          infoLocation: "5th Avenue Mansion",
