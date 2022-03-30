957d3e2882061f7b96f422a505a66d48a77b48b3
Neil Francis Hipona <neil.francis.hipona@codeandtheory.com>
2022-03-30 11:24:47 +0800

    Add new string bounds calcuation helper

--- SwiftUITraining/Models/String+Ext.swift
+++ SwiftUITraining/Models/String+Ext.swift
@@ -11,6 +11,13 @@ import UIKit
 
 extension String {
 
+    func constrainedSize(with canvas: CGSize, minHeight: CGFloat, font: UIFont) -> CGFloat {
+        let attributes: [NSAttributedString.Key: Any] = [.font: font]
+        let rect = self.boundingRect(with: canvas, options: [.usesLineFragmentOrigin, .usesFontLeading], attributes: attributes, context: nil)
+        let constrainedHeight = rect.height > minHeight ? rect.height : minHeight
+        return constrainedHeight
+    }
+
     func constrainedSize(with canvas: CGSize, minHeight: CGFloat, attributes: [NSAttributedString.Key: Any]) -> CGFloat {
         let rect = self.boundingRect(with: canvas, options: [.usesLineFragmentOrigin, .usesFontLeading], attributes: attributes, context: nil)
         let constrainedHeight = rect.height > minHeight ? rect.height : minHeight
