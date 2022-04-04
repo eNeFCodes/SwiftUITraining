7633e6f9c2592a370ff8d292e522b9ed87c92827
Neil Francis Hipona <neil.francis.hipona@codeandtheory.com>
2022-04-04 11:19:12 +0800

    Add ProductPageOverlayedView; add new assets

---
+++ SwiftUITraining/ProductPage/ProductPageOverlayedView.swift
@@ -0,0 +1,149 @@
+//
+//  ProductPageOverlayedView.swift
+//  SwiftUITraining
+//
+//  Created by Neil Francis Hipona on 4/4/22.
+//
+
+import SwiftUI
+
+extension ProductPageOverlayedView {
+    struct Model {
+        let contentDetails: String
+        let collectionTitle: String
+        let collectionDetails: String
+
+        let product: SearchPageView.Searched
+    }
+}
+
+struct ProductPageOverlayedView: View {
+    
+    let geometry: GeometryProxy
+    let model: Model
+
+    var body: some View {
+        VStack {
+            let sidePadding: CGFloat = 32
+            let detailFont = FontCollection.font(for: FontCollection.FancyCutProB7.regular(size: 18))
+            Text(model.contentDetails)
+                .accessibilityLabel(model.contentDetails)
+                .font(detailFont)
+                .foregroundColor(ColorCollection.black)
+                .padding(.leading, sidePadding)
+                .padding(.trailing, sidePadding)
+                .frame(width: abs(geometry.size.width), alignment: .leading)
+
+            ZStack {
+                VStack {
+                    buildTextContentStack(geometry: geometry, sidePadding: sidePadding)
+                    Spacer()
+                }
+
+                VStack {
+                    VStack {
+                        SearchResultItemView(geometry: geometry, item: model.product, sidePadding: sidePadding) { item in
+                            print("tapped: ", item)
+                        }
+                    }
+                    .padding(.top, 135)
+                    .frame(width: 150, alignment: .leading)
+                }
+                .frame(width: geometry.size.width, alignment: .trailing)
+            }
+            .padding(.top, 48)
+            .frame(width: geometry.size.width, alignment: .center)
+        }
+    }
+
+    private func buildTextContentStack(geometry: GeometryProxy, sidePadding: CGFloat) -> some View {
+        Group {
+            let collectionViewWidth = abs(geometry.size.width - (sidePadding * 2))
+            VStack {
+                VStack(spacing: 16) {
+                    let textContentWidth = collectionViewWidth - (sidePadding * 2)
+                    let titleFont = FontCollection.font(for: FontCollection.FancyCutCondProB7.bold(size: 30))
+                    Text(model.collectionTitle)
+                        .accessibilityLabel(model.collectionTitle)
+                        .foregroundColor(ColorCollection.white)
+                        .font(titleFont)
+                        .frame(width: textContentWidth, height: 80, alignment: .leading)
+
+                    let p1 = CGPoint(x: 0, y: 0)
+                    let p2 = CGPoint(x: textContentWidth, y: 0)
+                    BorderView(coordinates: [p1, p2])
+                        .stroke(style: StrokeStyle(lineWidth: 1, dash: [2]))
+                        .foregroundColor(ColorCollection.gold)
+                        .frame(width: textContentWidth, height: 1, alignment: .leading)
+
+                    HStack {
+                        let detailContentWidth = textContentWidth - 93
+                        let detailFont = FontCollection.font(for: FontCollection.FancyCutProB7.regular(size: 16))
+                        Text(model.collectionDetails)
+                            .accessibilityLabel(model.collectionDetails)
+                            .foregroundColor(ColorCollection.white)
+                            .font(detailFont)
+                            .frame(width: detailContentWidth, alignment: .leading)
+                    }
+                    .padding(.top, 32)
+                    .frame(width: textContentWidth, height: 190, alignment: .topLeading)
+
+                    HStack {
+                        Image("ic_3arrow_gold")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 40, height: 11, alignment: .leading)
+                    }
+                    .padding(.trailing, 110)
+                    .frame(width: textContentWidth, alignment: .trailing)
+                }
+                .padding(.top, 40)
+                .padding(.leading, sidePadding)
+                .padding(.trailing, sidePadding)
+                .frame(width: collectionViewWidth, alignment: .leading)
+
+                Spacer()
+            }
+            .frame(width: collectionViewWidth, height: 415, alignment: .center)
+            .background(ColorCollection.black)
+            .mask {
+                GeometryReader { geometry in
+                    let curve: CGFloat = 20
+                    let width = geometry.size.width
+                    let height = geometry.size.height
+
+                    let p1 = CGPoint(x: curve, y: 0)
+                    let p2 = CGPoint(x: width - curve, y: 0)
+                    let p3 = CGPoint(x: width, y: curve)
+                    let p4 = CGPoint(x: width, y: height - curve)
+                    let p5 = CGPoint(x: width - curve, y: height)
+                    let p6 = CGPoint(x: curve, y: height)
+                    let p7 = CGPoint(x: 0, y: height - curve)
+                    let p8 = CGPoint(x: 0, y: curve)
+                    BorderView(coordinates: [p1, p2, p3, p4, p5, p6, p7, p8], shouldClosePath: true)
+                }
+            }
+        }
+    }
+}
+
+struct ProductPageOverlayedView_Previews: PreviewProvider {
+    static var previews: some View {
+        GeometryReader { geometry in
+            ProductPageOverlayedView(geometry: geometry, model: ProductPageOverlayedView.mockData())
+        }
+    }
+}
+
+extension ProductPageOverlayedView {
+    static func mockData() -> ProductPageOverlayedView.Model {
+        .init(contentDetails: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec augue tempus erat luctus consequat. In venenatis varius imperdiet. Donec non massa eu enim dapibus ullamcorper. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec augue tempus erat luctus consequat. In venenatis varius imperdiet. Donec non massa eu enim dapibus ullamcorper.",
+              collectionTitle: "DISCOVER the collection".uppercased(),
+              collectionDetails: "Here's a selection of articles about Trinity Collection.",
+              product: .init(id: 0,
+                             title: "EYEBROW",
+                             subTitle: "A NEW CHAPTER ON NEW BOND STREET",
+                             imageName: "img_result5",
+                             date: "MAR. 1, 2020"))
+    }
+}
